90e1bc18455b5f6940eca8dd9af58e8ebc28153e
exh0306 <exh5452@psu.edu>
2026-03-02 22:46:52 -0500

    added screenshot of the code running

--- src/main/java/huang/lab3/Main.java
+++ src/main/java/huang/lab3/Main.java
@@ -1,4 +1,4 @@
-package huang.database.lab3;
+package huang.lab3;
 
 import huang.lab3.database.MongoDatabase;
 import huang.lab3.database.MySQLDatabase;
@@ -8,17 +8,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Customer c1 = new Customer(1, "Emmy", "Mi",
+        Customer c1 = new Customer(1, "Emmi", "Li",
                 "emmy@email.com", "22",
                 "111-1111", "New York",
                 "Gold", 100);
 
-        Customer c2 = new Customer(2, "Alice", "Smith",
+        Customer c2 = new Customer(2, "Allie", "Smith",
                 "alice@email.com", "30",
                 "222-2222", "Chicago",
                 "Silver", 50);
 
-        Customer c3 = new Customer(3, "Elaine", "Brown",
+        Customer c3 = new Customer(3, "Evan", "Brown",
                 "elaine@email.com", "28",
                 "333-3333", "Dallas",
                 "Platinum", 200);
